4c87abb5a8eb4f529128ff39f7ec79293d817894
jiayongfei <yongfeijia@tencent.com>
2020-11-02 16:48:31 +0800

    first commit
    second commit
    
    second commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit
    
    third commit

---
+++ test/test_test.go
@@ -0,0 +1,22 @@
+package test_test
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	"k8s.io/klog"
+	"os"
+)
+
+var _ = Describe("Test", func() {
+	It("test", func() {
+		fmt.Println("-----------")
+		data := os.Getenv("VERSION")
+		klog.Info(data)
+		if data == "a" {
+			fmt.Println("true")
+		}
+
+		fmt.Println("----------")
+		fmt.Println(os.Getenv("KUBECONFIG"))
+	})
+})
