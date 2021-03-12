2118422a6db0fb771986142b5883854ff64a06d1
jiayongfei <yongfeijia@tencent.com>
2021-03-12 16:16:27 +0800

    nnnn

--- test/test_test.go
+++ test/test_test.go
@@ -12,7 +12,7 @@ var _ = Describe("Test", func() {
 		fmt.Println("-----------")
 		data := os.Getenv("VERSION")
 		klog.Info(data)
-		klog.Info("newnewnewlalalalal")
+		klog.Info("newnewnewlalalalalooooooo")
 		if data == "a" {
 			fmt.Println("true")
 		}
